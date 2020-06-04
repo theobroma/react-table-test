2533d9cb5fca446ba0d7c6249ede40a9e8a6e998
Alexandr <theobroma222@gmail.com>
2020-06-04 19:38:38 +0300

    TableFC

--- src/App.tsx
+++ src/App.tsx
@@ -2,51 +2,19 @@ import React from 'react';
 import './assets/styles/index.scss';
 import './App.css';
 
-import {
-  Cards as JsCards,
-  CountryPicker,
-  Chart as JsChart,
-} from './components';
-import { fetchData } from './api';
-import styles from './App.module.css';
-import image from './assets/images/image.png';
-/* tslint:disable */
-const Cards: any = JsCards; // todo delete me after refactoring to TS
-const Chart: any = JsChart; // todo delete me after refactoring to TS
-/* tslint:enable */
+import { Table } from './components';
 
-class App extends React.Component {
-  state = {
-    data: {},
-    country: '',
-  };
-
-  async componentDidMount() {
-    const data = await fetchData();
-
-    this.setState({ data }, () => {
-      // console.log(this.state);
-    });
-  }
-
-  handleCountryChange = async (country: any) => {
-    const data = await fetchData(country);
-
-    this.setState({ data, country });
-  };
-
-  render() {
-    const { data, country } = this.state;
-
-    return (
-      <div className={styles.container}>
-        <img className={styles.image} src={image} alt="COVID-19" />
-        <Cards data={data} />
-        <CountryPicker handleCountryChange={this.handleCountryChange} />
-        <Chart data={data} country={country} />
-      </div>
-    );
-  }
+interface Props {
+  //   activeTodoCount: number;
 }
+
+const App: React.FC<Props> = React.memo((props) => {
+  return (
+    <div>
+      <h1>Title</h1>
+      <Table />
+    </div>
+  );
+});
 
 export default App;
---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface Props {
+  //   activeTodoCount: number;
+}
+
+const Table: React.FC<Props> = React.memo((props) => {
+  return (
+    <div>
+      123123
+      <div>Table</div>
+    </div>
+  );
+});
+
+export default Table;
